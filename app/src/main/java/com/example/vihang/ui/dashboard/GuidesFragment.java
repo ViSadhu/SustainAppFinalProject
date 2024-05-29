332da7419cb447f74507c7eb9bcadfd29bc9f486
visadhu <vihangsadhu@gmail.com>
2024-05-29 01:48:10 -0600

    Guide Fragment Finished

--- app/src/main/java/com/example/vihang/ui/dashboard/GuidesFragment.java
+++ app/src/main/java/com/example/vihang/ui/dashboard/GuidesFragment.java
@@ -10,22 +10,18 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.vihang.databinding.FragmentDashboardBinding;
+import com.example.vihang.databinding.FragmentGuidesBinding;
 
-public class DashboardFragment extends Fragment {
+public class GuidesFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentGuidesBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentGuidesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
